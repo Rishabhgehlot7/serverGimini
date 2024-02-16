d62281f89ae16932ddb83cfa2cfbf3030fe830af
rishabh gehlot <rishabh3x@gmail.com>
2024-02-16 17:27:55 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,49 @@
+import express from 'express'
+import * as dotenv from 'dotenv'
+import cors from 'cors'
+// import { Configuration, OpenAIApi } from 'openai'
+
+
+dotenv.config()
+
+// const configuration = new Configuration({
+//   apiKey: process.env.OPENAI_API_KEY,
+// });
+
+// const openai = new OpenAIApi(configuration);
+
+import { GoogleGenerativeAI } from '@google/generative-ai'
+
+// Access your API key as an environment variable (see "Set up your API key" above)
+const genAI = new GoogleGenerativeAI('AIzaSyBHDHdUhlu83sVCy2Q8Ul4ELcry_JAImWU');
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+
+app.get('/', async (req, res) => {
+  res.status(200).send({
+    message: 'Hello from CodeX!'
+  })
+})
+
+app.post('/', async (req, res) => {
+  try {
+    const prompt = req.body.prompt;
+
+    const model = genAI.getGenerativeModel({ model: "gemini-pro" });
+    const result = await model.generateContent(prompt);
+    const response = await result.response;
+    const text = response.text();
+
+    res.status(200).send({
+      bot: text
+    });
+
+  } catch (error) {
+    console.error(error)
+    res.status(500).send(error || 'Something went wrong');
+  }
+})
+
+app.listen(5000, () => console.log('AI server started on http://localhost:5000'))
